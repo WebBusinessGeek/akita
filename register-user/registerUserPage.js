29bfe0e59e1bfc47bb5ed8f1dd53d1fa5502f386
Kevin <kevw12188@gmail.com>
2016-05-11 17:18:56 -0400

    built form for register user flow

--- register-user/registerUserPage.js
+++ register-user/registerUserPage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import BasePage from "./../shared/extendables/BasePage"
 import {REGISTER_USER_ENDPOINT} from "./../shared/constants/apiEndpoints"
+import {Link} from "react-router"
 
 export default class RegisterUserPage extends BasePage {
     constructor() {
@@ -22,9 +23,24 @@ export default class RegisterUserPage extends BasePage {
 
     render() {
         return(
-            <div>
-                <p>This is the register user components</p>
-                <button class="btn btn-primary" onClick={this.check}>Click</button>
+            <div id="register-user-form-container">
+                <p id="register-user-form-blurb">Create your account</p>
+                <form id="register-user-form">
+                    <div id="register-user-form-email-group" className="form-group">
+                        <label htmlFor="emailInput">Email</label>
+                        <input type="email" className="form-control" id="emailInput" placeholder="" />
+                    </div>
+                    <div id="register-user-form-password-group" className="form-group">
+                        <label htmlFor="passwordInput">Password</label>
+                        <input type="password" className="form-control" id="passwordInput" placeholder="" />
+                    </div>
+                    <div id="register-user-form-confirm-password-group" className="form-group">
+                        <label htmlFor="confirmPasswordInput">Confirm Password</label>
+                        <input type="password" className="form-control" id="confirmPasswordInput" placeholder="" />
+                    </div>
+                    <button className="btn btn-lg btn-primary" onClick={this.check}>Click</button>
+                </form>
+                <p>Already have an account? <Link to={`/login`}><button id="log_in_link_btn" className="btn btn-default">Log In</button></Link></p>
             </div>
         )
     }
