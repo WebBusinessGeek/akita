9eff2829042f99d77f918a6cbbab26d768c99664
Kevin <kevw12188@gmail.com>
2016-05-11 16:14:16 -0400

    rmvl of strings from app.js file

--- core/app.js
+++ core/app.js
@@ -1,21 +1,21 @@
 import express from "express"
 import Root from "./../rootdirectory"
+import cors from "cors"
+import {BUILD_DIRECTORY} from "./../shared/constants/directory"
+import {LOCAL_ADDR} from "./../shared/constants/transport"
 
 /*instantiate the express app*/
 const APP = express()
 
-
-import cors from "cors"
-
-
+/*allow cors*/
 let corsOptions = {
-    origin: "http://localhost:8080",
+    origin: LOCAL_ADDR,
     credentials: true
 }
-
 APP.use(cors(corsOptions))
-/*set static directory*/
-APP.use(express.static(Root + "/build"))
 
-/*export the app*/
+/*set static directory*/
+APP.use(express.static(BUILD_DIRECTORY))
+
+/*export the app instance*/
 export default APP
---
+++ shared/constants/transport.js
@@ -0,0 +1,3 @@
+export const LOCALHOST = "http://localhost"
+export const LOCAL_PORT = 8080
+export const LOCAL_ADDR = LOCALHOST + LOCAL_PORT
