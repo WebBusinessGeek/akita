b86cafe9ef01879be41d356fa9c3154b5df19595
Kevin <kevw12188@gmail.com>
2016-05-12 12:49:46 -0400

    added more styling to registerUser page

--- register-user/registerUserPage.js
+++ register-user/registerUserPage.js
@@ -41,9 +41,9 @@ export default class RegisterUserPage extends BasePage {
                         <label htmlFor="confirmPasswordInput"></label>
                         <input type="password" className="form-control register-user-form-inputs" id="confirmPasswordInput" placeholder="Confirm It" />
                     </div>
-                    <button id="submit" className="btn btn-lg btn-primary primaryBtn" onClick={this.check}>Create Account</button>
+                    <button id="submit" className="btn btn-lg btn-primary greenBtn" onClick={this.check}>Create Account</button>
                 </form>
-                <p>Already have an account? <Link to={`/login`}><button id="log_in_link_btn" className="btn btn-default">Log In</button></Link></p>
+                <p>Already have an account? <Link to={`/login`}><button id="log_in_link_btn" className="btn btn-default pinkBtn">Log In</button></Link></p>
             </div>
         )
     }
