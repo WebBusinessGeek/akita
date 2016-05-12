db02335d54e554f845610c083067a35a2db4beff
Kevin <kevw12188@gmail.com>
2016-05-12 11:58:15 -0400

    added base style sheet along with some variables

--- register-user/registerUserPage.js
+++ register-user/registerUserPage.js
@@ -41,7 +41,7 @@ export default class RegisterUserPage extends BasePage {
                         <label htmlFor="confirmPasswordInput"></label>
                         <input type="password" className="form-control register-user-form-inputs" id="confirmPasswordInput" placeholder="Confirm It" />
                     </div>
-                    <button className="btn btn-lg btn-primary" onClick={this.check}>Click</button>
+                    <button id="submit" className="btn btn-lg btn-primary primaryBtn" onClick={this.check}>Create Account</button>
                 </form>
                 <p>Already have an account? <Link to={`/login`}><button id="log_in_link_btn" className="btn btn-default">Log In</button></Link></p>
             </div>
