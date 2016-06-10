93e586130631d0c094b1aa2117ffb170bf6efb8b
Kevin <kevw12188@gmail.com>
2016-06-10 10:12:45 -0400

    more work on validating tokens

--- shared/services/TokenValidationHandler.js
+++ shared/services/TokenValidationHandler.js
@@ -1,1 +1,14 @@
-/*set up a router*/
+import {newRouter, parser} from "./../services/APIRoutingService"
+import {successResponse, errorResponse, failResponse} from "./../services/APIResponseService"
+import {MISSING_TOKEN_ERROR} from  "./../constants/notifications"
+
+let router = newRouter()
+
+router.post("/", parser.array(), (req, res) => {
+    let token = req.body.token
+
+    if(!token) {
+        return res.json(failResponse(MISSING_TOKEN_ERROR))
+    }
+//    verify the token (ALSO should there be a TokenProviderService??? i think so!
+})
