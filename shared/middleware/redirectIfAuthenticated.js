ff544e14f66d2d764b660c897f814233271d2b25
Kevin <kevw12188@gmail.com>
2016-06-16 12:55:13 -0400

    clean

--- shared/middleware/redirectIfAuthenticated.js
+++ shared/middleware/redirectIfAuthenticated.js
@@ -1,10 +1,11 @@
 import AuthenticationCheckService from "./../services/AuthenticationCheckService"
+import {DASHBOARD_ROUTE} from "./../constants/clientRoutes"
 
 export default function redirectIfAuthenticated(nextState, replace, cb) {
     let authCheck = new AuthenticationCheckService()
     authCheck.isAuthenticatedAsync()
         .then((auth) => {
-            (auth) ? replace("/dashboard") : false
+            (auth) ? replace(DASHBOARD_ROUTE) : false
             cb()
         })
         .catch((err) => {
