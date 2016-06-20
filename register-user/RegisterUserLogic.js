034ba14a34d37e0c5fbdc39a1ede3735f87febb1
Kevin <kevw12188@gmail.com>
2016-06-20 09:50:32 -0400

    abstracted logic from apiHandler

---
+++ register-user/RegisterUserLogic.js
@@ -0,0 +1,67 @@
+import {successResponse, errorResponse, failResponse} from "./../shared/services/APIResponseService"
+import {MISSING_EMAIL_ERROR, MISSING_PASSWORD_ERROR, INVALID_EMAIL_FORMAT_ERROR,
+    INVALID_PASSWORD_FORMAT_ERROR, USER_ALREADY_EXISTS_ERROR, INVALID_EMAIL_ERROR, USER_REGISTRATION_SUCCESS} from "./../shared/constants/notifications"
+import {MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH} from "../shared/constants/auth"
+import validator from "validator"
+import User from "./../shared/db/models/User"
+import bcrypt from "bcrypt"
+import EmailValidator from "./../shared/services/EmailValidatorService"
+import TokenProvider from "./../shared/services/TokenProviderService"
+
+
+export default class RegisterUserLogic {
+    constructor(res) {
+        this.res = res
+    }
+    
+    run(email, password) {
+        if(!email) {
+            this.jsonResponse(failResponse(MISSING_EMAIL_ERROR))
+        }
+        if(!password) {
+            this.jsonResponse(failResponse(MISSING_PASSWORD_ERROR))
+        }
+        if(!validator.isEmail(email)) {
+            this.jsonResponse(failResponse(INVALID_EMAIL_ERROR))
+        }
+        if(!validator.isLength(password, {min : MIN_PASSWORD_LENGTH, max : MAX_PASSWORD_LENGTH}) || !validator.isAlphanumeric(password)) {
+            this.jsonResponse(failResponse(INVALID_EMAIL_FORMAT_ERROR))
+        }
+        this.attemptToRegisterUser(email, password)
+    }
+
+    jsonResponse(response) {
+        return this.res.json(response)
+    }
+    
+    attemptToRegisterUser(email, password) {
+        User.findOne({where: {email: email}})
+            .then((user) => {
+                if(user) {
+                    return this.jsonResponse(failResponse(USER_ALREADY_EXISTS_ERROR))
+                }
+                let emailValidator = new EmailValidator()
+                emailValidator.verifyAsync(email)
+                    .then((resp) => {
+                        if(!resp.format_valid || !resp.smtp_check) {
+                            return this.jsonResponse(failResponse(INVALID_EMAIL_ERROR))
+                        } else {
+                            User.create({
+                                email: email,
+                                password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+                            })
+                                .then((user) => {
+                                    let tokenProvider = new TokenProvider()
+                                    let token = tokenProvider.newToken(user)
+                                    return this.jsonResponse(successResponse(USER_REGISTRATION_SUCCESS, {token : token}))
+                                })
+                        }
+                    })
+            })
+            .catch((err) => {
+                return this.jsonResponse(errorResponse(err))
+            })
+    }
+
+    
+}
--- register-user/registerUserAPIHandler.js
+++ register-user/registerUserAPIHandler.js
@@ -1,58 +1,14 @@
 import {newRouter, parser} from "./../shared/services/APIRoutingService"
-import {successResponse, errorResponse, failResponse} from "./../shared/services/APIResponseService"
-import {MISSING_EMAIL_ERROR, MISSING_PASSWORD_ERROR, INVALID_EMAIL_FORMAT_ERROR,
-    INVALID_PASSWORD_FORMAT_ERROR, USER_ALREADY_EXISTS_ERROR, INVALID_EMAIL_ERROR, USER_REGISTRATION_SUCCESS} from "./../shared/constants/notifications"
-import {MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH} from "../shared/constants/auth"
-import validator from "validator"
-import User from "./../shared/db/models/User"
-import bcrypt from "bcrypt"
-import EmailValidator from "./../shared/services/EmailValidatorService"
-import TokenProvider from "./../shared/services/TokenProviderService"
+import RegisterUserLogic from "./RegisterUserLogic"
 
 let router = newRouter()
 
 router.post("/", parser.array(), (req, res) => {
     let email = req.body.email
     let password = req.body.password
-
-    if(!email) {
-        return res.json(failResponse(MISSING_EMAIL_ERROR))
-    }
-    if(!password) {
-        return res.json(failResponse(MISSING_PASSWORD_ERROR))
-    }
-    if(!validator.isEmail(email)) {
-        return res.json(failResponse(INVALID_EMAIL_FORMAT_ERROR))
-    }
-    if(!validator.isLength(password, {min : MIN_PASSWORD_LENGTH, max : MAX_PASSWORD_LENGTH}) || !validator.isAlphanumeric(password)) {
-        return res.json(failResponse(INVALID_PASSWORD_FORMAT_ERROR))
-    }
-    User.findOne({where: {email: email}})
-        .then((user) => {
-            if(user) {
-                return res.json(failResponse(USER_ALREADY_EXISTS_ERROR))
-            }
-            let emailValidator = new EmailValidator()
-            emailValidator.verifyAsync(email)
-                .then((resp) => {
-                    if(!resp.format_valid || !resp.smtp_check) {
-                        return res.json(failResponse(INVALID_EMAIL_ERROR))
-                    } else {
-                        User.create({
-                            email: email,
-                            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-                        })
-                            .then((user) => {
-                                let tokenProvider = new TokenProvider()
-                                let token = tokenProvider.newToken(user)
-                                return res.json(successResponse(USER_REGISTRATION_SUCCESS, {token : token}))
-                            })
-                    }
-                })
-        })
-        .catch((err) => {
-            return res.json(errorResponse(err))
-        })
+    
+    let logic = new RegisterUserLogic(res)
+    logic.run(email, password)
 })
 
 export default router
