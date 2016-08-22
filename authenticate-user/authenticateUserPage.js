05488e7dcc92d548621f4b5fefcd04df46ec8d62
Kevin <kevw12188@gmail.com>
2016-08-22 11:39:26 -0400

    drying up css

--- authenticate-user/authenticateUserPage.js
+++ authenticate-user/authenticateUserPage.js
@@ -1,3 +1,39 @@
-/**
- * Created by kevinwashington on 8/22/16.
- */
+require("./authenticateUserStyling.scss")
+import React from "react"
+import ReactDOM from "react-dom"
+import BasePage from "./../shared/extendables/BasePage"
+import {Link} from "react-router"
+
+
+export default class AuthenticateUserPage extends BasePage {
+    constructor() {
+        super()
+        this.state = {
+            errorMessage : null
+        }
+    }
+
+    render() {
+        return(
+            <div id="authenticate-user-form-container" className="user-form-container">
+                <form id="authenticate-user-form" className="text-center">
+                    <h2 id="authenticate-user-form-heading" className="boldText">akita</h2>
+                    <p id="authenticate-user-form-blurb" className="greyText">Sign into your account below.</p>
+                    <div>{this.state.errorMessage != null ? this.renderErrorMessage() : false}</div>
+                    <div id="authenticate-user-form-email-group" className="form-group">
+                        <label htmlFor="emailInput"></label>
+                        <input type="email" className="form-control user-form-inputs" ref="emailInput" id="emailInput" placeholder="Email" />
+                    </div>
+                    <div id="authenticate-user-form-password-group" className="form-group">
+                        <label htmlFor="passwordInput"></label>
+                        <input type="password" className="form-control user-form-inputs" ref="passwordInput" id="passwordInput" placeholder="Password" />
+                    </div>
+                    <button id="authenticateBtn" className="btn user-form-main-btn greenBtn" onClick={this.handleSubmit}>Sign in</button>
+                </form>
+                <div id="create-account-button-container" className="user-form-secondary-btn-container">
+                    <p>Don't have an account yet? <Link to={`/`}><button id="create-account-btn-link" className="btn user-form-secondary-btn pinkBtn">Create my account</button></Link></p>
+                </div>
+            </div>
+        )
+    }
+}
